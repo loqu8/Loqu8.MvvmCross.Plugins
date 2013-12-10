eb3cf1305c38aa8828a8bcce09a5c0a42e3daaba
Tim Uy <tim@loqu8.com>
2013-12-10 14:07:45 -0800

    Added MvxPluginTemplate (pre-substitution)

---
+++ template/MvxPluginTemplate/IMvxTemplate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Loqu8.MvvmCross.Plugins.$safeprojectname$
+{
+    public interface IMvxTemplate
+    {
+
+    }
+}
---
+++ template/MvxPluginTemplate/Shared Files/MvxTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Loqu8.MvvmCross.Plugins.$safeprojectname$
+{
+    public class MvxTemplate : IMvxTemplate
+    {        
+        public MvxTemplate()
+        {
+        }
+
+        #region ITemplate
+
+        #endregion ITemplate
+    }
+}
