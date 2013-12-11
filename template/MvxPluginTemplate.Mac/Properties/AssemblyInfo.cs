b1043c1f05679b4edfe7275d6514940134c8839b
Tim Uy <tim@loqu8.com>
2013-12-11 00:09:38 -0800

    Templates closer but still errors with Reference Path and Shared File

--- template/MvxPluginTemplate.Mac/Properties/AssemblyInfo.cs
+++ template/MvxPluginTemplate.Mac/Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Loqu8.MvvmCross.Plugins.$safeprojectname$)]
-[assembly: AssemblyDescription("Loqu8.MvvmCross.Plugins.$safeprojectname$)]
-[assembly: AssemblyConfiguration("Loqu8.MvvmCross.Plugins.$safeprojectname$)]
-[assembly: AssemblyCompany("Loqu8.MvvmCross.Plugins.$safeprojectname$)]
-[assembly: AssemblyProduct("Loqu8.MvvmCross.Plugins.$safeprojectname$)]
-[assembly: AssemblyCopyright("Loqu8.MvvmCross.Plugins.$safeprojectname$)]
-[assembly: AssemblyTrademark("Loqu8.MvvmCross.Plugins.$safeprojectname$)]
-[assembly: AssemblyCulture("Loqu8.MvvmCross.Plugins.$safeprojectname$)]
+[assembly: AssemblyTitle("Loqu8.MvvmCross.Plugins.$safeprojectname$")]
+[assembly: AssemblyDescription("Loqu8.MvvmCross.Plugins.$safeprojectname$")]
+[assembly: AssemblyConfiguration("Loqu8.MvvmCross.Plugins.$safeprojectname$")]
+[assembly: AssemblyCompany("Loqu8.MvvmCross.Plugins.$safeprojectname$")]
+[assembly: AssemblyProduct("Loqu8.MvvmCross.Plugins.$safeprojectname$")]
+[assembly: AssemblyCopyright("Loqu8.MvvmCross.Plugins.$safeprojectname$")]
+[assembly: AssemblyTrademark("Loqu8.MvvmCross.Plugins.$safeprojectname$")]
+[assembly: AssemblyCulture("Loqu8.MvvmCross.Plugins.$safeprojectname$")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("Loqu8.MvvmCross.Plugins.$safeprojectname$)]
+[assembly: Guid("Loqu8.MvvmCross.Plugins.$safeprojectname$")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("Loqu8.MvvmCross.Plugins.$safeprojectname$)]
-[assembly: AssemblyVersion("Loqu8.MvvmCross.Plugins.$safeprojectname$)]
-[assembly: AssemblyFileVersion("Loqu8.MvvmCross.Plugins.$safeprojectname$)]
+// [assembly: AssemblyVersion("Loqu8.MvvmCross.Plugins.$safeprojectname$")]
+[assembly: AssemblyVersion("Loqu8.MvvmCross.Plugins.$safeprojectname$")]
+[assembly: AssemblyFileVersion("Loqu8.MvvmCross.Plugins.$safeprojectname$")]
